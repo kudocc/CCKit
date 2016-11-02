2b7a8e797474044a611a41a77e4fb37b5c987d1f
KudoCC <cangmuma@gmail.com>
2016-11-02 16:07:07 +0800

    update

--- CCKitDemo/quartz2D/CoordinateViewController.m
+++ CCKitDemo/quartz2D/CoordinateViewController.m
@@ -15,36 +15,48 @@
     
     UIViewCordinate *v = [[UIViewCordinate alloc] initWithFrame:CGRectMake(0, 64, ScreenWidth, ScreenHeight-64)];
     [self.view addSubview:v];
+    
+    /*
+     假设整个区域的大小是200*200px，scale是2，srcPoint所在位置为(10, 10)point，被转换成Quartz的坐标系的位置应该是(20, 180)px
+     srcPoint位于UIKit的坐标系中，UIKit要使用仿射变换使其变成Quartz的坐标系，我们模拟了这个过程
+     在看代码之前，我们要考虑Quartz的坐标系是什么样的，我觉得Quartz应该跟`CGBitmapContextCreate`创建的context中使用的坐标系是一样的
+     而这个context，我用`CGContextGetCTM(context)`拿到的坐标系仿射变换的值为[1, 0, 0, 1, 0, 0]
+     因为UIKit中使用的是point，并且是以左上角为原点，所以UIKit要应用仿射变换将scrPoint的点变成同样位置的Quartz的点
+     Height为区域的大小，单位是point
+     如果scale为1，将srcPoint转换成Quartz的位置应该是 (srcPoint.x, Height - srcPoint.y)
+     如果scale大于1，那么应该是(srcPoint.x * scale, (Height - srcPoint.y) * scale)
+     所以可以做如下仿射变换，这应该就是UIKit创建CGContext时对CGContext做的仿射变换
+     这里要注意的是这个函数以及相关的对transform做变换的函数，以CGAffineTransformTranslate(transform, x, y)为例
+     他是将CGAffineTransformMakeTranslation(x, y)的结果与transform做矩阵相乘
+     而不是transform与CGAffineTransformMakeTranslation(x, y)的结果相乘，矩阵相乘交换律不成立，所以千万小心
+     */
+    
+    CGPoint srcPoint = CGPointMake(10, 10);
+    CGFloat scale = [UIScreen mainScreen].scale;
+    if (scale > 1) {
+        CGAffineTransform transform = CGAffineTransformMakeScale(scale, scale);
+        NSLog(@"%@", NSStringFromCGAffineTransform(transform));
+        
+        transform = CGAffineTransformTranslate(transform, 0, 100);
+        NSLog(@"%@", NSStringFromCGAffineTransform(transform));
+        
+        transform = CGAffineTransformScale(transform, 1, -1);
+        NSLog(@"%@", NSStringFromCGAffineTransform(transform));
+        
+        CGPoint dstPoint = CGPointApplyAffineTransform(srcPoint, transform);
+        NSLog(@"src point:%@", NSStringFromCGPoint(srcPoint));
+        NSLog(@"det point:%@", NSStringFromCGPoint(dstPoint));
+    } else {
+        CGAffineTransform transform = CGAffineTransformMakeScale(1/scale, 1/scale);
+        NSLog(@"%@", NSStringFromCGAffineTransform(transform));
+        transform = CGAffineTransformScale(transform, 1, -1);
+        NSLog(@"%@", NSStringFromCGAffineTransform(transform));
+        transform = CGAffineTransformTranslate(transform, 0, -200);
+        NSLog(@"%@", NSStringFromCGAffineTransform(transform));
+    }
 }
 
 @end
-
-void MyDrawWithShadows (CGContextRef myContext, CGFloat wd, CGFloat ht)
-{
-    CGSize          myShadowOffset = CGSizeMake (-15,  20);// 2
-    CGFloat           myColorValues[] = {1, 0, 0, .6};// 3
-    CGColorRef      myColor;// 4
-    CGColorSpaceRef myColorSpace;// 5
-    
-    CGContextSaveGState(myContext);// 6
-    
-    CGContextSetShadow (myContext, myShadowOffset, 5); // 7
-    // Your drawing code here// 8
-    CGContextSetRGBFillColor (myContext, 0, 1, 0, 1);
-    CGContextFillRect (myContext, CGRectMake (wd/3 + 75, ht/2 , wd/4, ht/4));
-    
-    myColorSpace = CGColorSpaceCreateDeviceRGB ();// 9
-    myColor = CGColorCreate (myColorSpace, myColorValues);// 10
-    CGContextSetShadowWithColor (myContext, myShadowOffset, 5, myColor);// 11
-    // Your drawing code here// 12
-    CGContextSetRGBFillColor (myContext, 0, 0, 1, 1);
-    CGContextFillRect (myContext, CGRectMake (wd/3-75,ht/2-100,wd/4,ht/4));
-    
-    CGColorRelease (myColor);// 13
-    CGColorSpaceRelease (myColorSpace); // 14
-    
-    CGContextRestoreGState(myContext);// 15
-}
 
 @implementation UIViewCordinate {
     UIImage *image;
@@ -58,7 +70,7 @@ void MyDrawWithShadows (CGContextRef myContext, CGFloat wd, CGFloat ht)
         image = [UIImage imageNamed:@"ori"];
         
         path = CGPathCreateMutable();
-        CGPathAddArc(path, NULL, 100, 100, 100, 0, M_PI/4, YES);
+        CGPathAddArc(path, NULL, 100, 100, 100, 0, M_PI/2, YES);
         CGPathCloseSubpath(path);
         bezierPath = [UIBezierPath bezierPathWithCGPath:path];
     }
@@ -70,48 +82,22 @@ void MyDrawWithShadows (CGContextRef myContext, CGFloat wd, CGFloat ht)
 }
 
 - (void)drawRect:(CGRect)rect {
-    
-    CGFloat y = 64;
-    [image drawInRect:CGRectMake(0, y, image.size.width, image.size.height)];
-    
+    // 使用UIBezierPath和使用其path直接用CGContextDrawPath效果是一样的
     CGContextRef context = UIGraphicsGetCurrentContext();
-    MyDrawWithShadows(context, 100, 100);
-    {
-        CGContextSetStrokeColorWithColor(context, [UIColor redColor].CGColor);
-        path = CGPathCreateMutable();
-        CGPathAddArc(path, NULL, 100, 100, 100, 0, M_PI/4, YES);
-        CGPathCloseSubpath(path);
-        CGContextAddPath(context, path);
-        CGContextDrawPath(context, kCGPathStroke);
-        [bezierPath stroke];
-    }
+    CGContextSetStrokeColorWithColor(context, [UIColor redColor].CGColor);
+    CGContextAddPath(context, path);
+    CGContextDrawPath(context, kCGPathStroke);
     
+    [bezierPath stroke];
     
+    // 使用CGContextDrawImage正确绘制图片
+    CGFloat y = 200.0;
     NSLog(@"%@", NSStringFromCGAffineTransform(CGContextGetCTM(context)));
-    y += image.size.height;
     CGContextSaveGState(context);
     CGContextTranslateCTM(context, 0, y+image.size.height);
     CGContextScaleCTM(context, 1.0, -1.0);
     CGContextDrawImage(context, CGRectMake(0, 0, image.size.width, image.size.height), image.CGImage);
     CGContextRestoreGState(context);
-    
-    // flip y
-    CGContextTranslateCTM(context, 0, self.bounds.size.height);
-    CGContextScaleCTM(context, 1, -1);
-    NSLog(@"%@", NSStringFromCGAffineTransform(CGContextGetCTM(context)));
-    CGContextDrawImage(context, CGRectMake(0, 0, image.size.width, image.size.height), image.CGImage);
-    
-    {
-        CGContextSetStrokeColorWithColor(context, [UIColor redColor].CGColor);
-        path = CGPathCreateMutable();
-        CGPathAddArc(path, NULL, 100, 100, 100, 0, M_PI/4, YES);
-        CGPathCloseSubpath(path);
-        CGContextAddPath(context, path);
-        CGContextDrawPath(context, kCGPathStroke);
-        [bezierPath stroke];
-    }
-    
-    MyDrawWithShadows(context, 100, 100);
 }
 
 @end
--- CCKitDemo/quartz2D/PathViewController.m
+++ CCKitDemo/quartz2D/PathViewController.m
@@ -11,86 +11,7 @@
 
 @implementation PathViewController
 
-/**
- 在context上绘制一个subpath，不绘制这个subpath，然后调用CGContextMoveToPoint，那么之前的path会被绘制上去吗
- 
- 结果是可以绘制上去，之前的path是context的subpath，调用CGContextMoveToPoint开始了一个新的subpath
- */
-- (UIImage *)imageTest1WithSize:(CGSize)size {
-    UIGraphicsBeginImageContextWithOptions(size, NO, 0);
-    CGContextRef context = UIGraphicsGetCurrentContext();
-    CGContextSetStrokeColorWithColor(context, [UIColor redColor].CGColor);
-    CGContextMoveToPoint(context, size.width/2, size.height/2);
-    CGContextAddArc(context, size.width/2, size.height/2, floor(size.width/2), 0, M_PI/2, 1);
-    
-    CGContextMoveToPoint(context, size.width, size.height);
-    CGContextAddLineToPoint(context, 0, 0);
-    CGContextStrokePath(context);
-    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
-    UIGraphicsEndImageContext();
-    return image;
-}
-
-/**
- 在context上绘制一个subpath，不绘制这个subpath，然后调用类似`CGContextStrokeRect`的方法，那么这个path会被绘制上去么?
- 
- 结果是没有绘制之前的subpath，`CGContextStrokeRect`的Discussion中解释：As a side effect when you call this function, Quartz clears the current path，所以除非在调用这个方法之前调用`CGContextStrokeRect`，否则不会绘制当前的path了。
- */
-- (UIImage *)imageTest2WithSize:(CGSize)size {
-    UIGraphicsBeginImageContextWithOptions(size, NO, 0);
-    CGContextRef context = UIGraphicsGetCurrentContext();
-    CGContextSetStrokeColorWithColor(context, [UIColor redColor].CGColor);
-    CGContextMoveToPoint(context, size.width/2, size.height/2);
-    CGContextAddArc(context, size.width/2, size.height/2, floor(size.width/2), 0, M_PI/2, 1);
-    
-    CGRect rect = CGRectMake(0, 0, size.width, size.height);
-    rect = CGRectInset(rect, 2, 2);
-    CGContextStrokeRect(context, rect);
-    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
-    UIGraphicsEndImageContext();
-    return image;
-}
-
-// CGContext的绘制都是先依照Quartz的坐标系绘制，然后根据当前context的CTM，将坐标变换成目标值
-
-/**
- 调用`CGContextFillPath`方法会自动视当前的path为调用CGContextClosePath的结果
- */
-- (UIImage *)imageTestAutoClosePathWithSize:(CGSize)size {
-    UIGraphicsBeginImageContextWithOptions(size, NO, 0);
-    CGContextRef context = UIGraphicsGetCurrentContext();
-    CGContextSetStrokeColorWithColor(context, [UIColor redColor].CGColor);
-    
-    // 这里如果不调用这个方法，自动close path的时候会视start point为arc的开始点
-    CGContextMoveToPoint(context, size.width/2, size.height/2);
-    
-    CGContextAddArc(context, size.width/2, size.height/2, floor(size.width/2), 0, M_PI/2, 1);
-    CGContextFillPath(context);
-    
-    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
-    UIGraphicsEndImageContext();
-    return image;
-}
-
-- (UIImage *)imageTestArcWithSize:(CGSize)size {
-    UIGraphicsBeginImageContextWithOptions(size, NO, 0);
-    CGContextRef context = UIGraphicsGetCurrentContext();
-    CGContextSetStrokeColorWithColor(context, [UIColor redColor].CGColor);
-    
-    CGRect rect = CGRectMake(0, 0, size.width, size.height/2);
-    CGFloat radius = rect.size.height/2;
-    CGContextMoveToPoint(context, radius, 0);
-    CGContextAddArcToPoint(context, 0, 0, 0, radius, radius/2);
-    
-    CGContextMoveToPoint(context, rect.size.width-radius, 0);
-    CGContextAddArcToPoint(context, rect.size.width, 0, rect.size.width, radius, radius);
-    CGContextStrokePath(context);
-    
-    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
-    UIGraphicsEndImageContext();
-    return image;
-}
-
+/// 一个稍微复杂的绘制
 - (UIImage *)imagePieChartWithPercentArray:(NSArray *)percents colors:(NSArray *)colors imageSize:(CGSize)size {
     UIGraphicsBeginImageContextWithOptions(size, NO, 0);
     CGContextRef context = UIGraphicsGetCurrentContext();
@@ -137,6 +58,117 @@
     return image;
 }
 
+
+/**
+ 在context上绘制一个subpath，不绘制这个subpath，然后调用CGContextMoveToPoint，那么之前的path会被绘制上去吗
+ 
+ 结果是可以绘制上去，之前的path是context的subpath，调用CGContextMoveToPoint开始了一个新的subpath
+ */
+- (UIImage *)imageTest1WithSize:(CGSize)size {
+    UIGraphicsBeginImageContextWithOptions(size, NO, 0);
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    CGContextSetStrokeColorWithColor(context, [UIColor redColor].CGColor);
+    
+    CGContextMoveToPoint(context, size.width/2, size.height/2);
+    CGContextAddArc(context, size.width/2, size.height/2, floor(size.width/2), 0, M_PI/2, 1);
+    
+    CGContextMoveToPoint(context, size.width, size.height);
+    CGContextAddLineToPoint(context, 0, 0);
+    CGContextStrokePath(context);
+    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    return image;
+}
+
+- (UIImage *)imageTest1BitmapContextWithSize:(CGSize)size {
+    
+    CGFloat scale = [UIScreen mainScreen].scale;
+    size = CGSizeApplyAffineTransform(size, CGAffineTransformMakeScale(scale, scale));
+    
+    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+    CGContextRef context = CGBitmapContextCreate(NULL, size.width, size.height, 8, size.width * 4, colorSpace, kCGImageAlphaNoneSkipLast);
+    
+    CGContextSetFillColorWithColor(context, [UIColor clearColor].CGColor);
+    CGContextFillRect(context, CGRectMake(0, 0, size.width, size.height));
+    CGContextSetStrokeColorWithColor(context, [UIColor redColor].CGColor);
+    
+    CGContextMoveToPoint(context, size.width/2, size.height/2);
+    CGContextAddArc(context, size.width/2, size.height/2, floor(size.width/2), 0, M_PI/2, 1);
+    
+    CGContextMoveToPoint(context, size.width, size.height);
+    CGContextAddLineToPoint(context, 0, 0);
+    CGContextStrokePath(context);
+    
+    CGImageRef imageRes = CGBitmapContextCreateImage(context);
+    UIImage *image = [UIImage imageWithCGImage:imageRes scale:[UIScreen mainScreen].scale orientation:UIImageOrientationUp];
+    CGImageRelease(imageRes);
+    CGColorSpaceRelease(colorSpace);
+    CGContextRelease(context);
+//    CGPathRelease(mPath);
+    
+    return image;
+}
+
+/**
+ 在context上绘制一个subpath，不绘制这个subpath，然后调用类似`CGContextStrokeRect`的方法，那么这个path会被绘制上去么?
+ 
+ 结果是没有绘制之前的subpath，`CGContextStrokeRect`的Discussion中解释：As a side effect when you call this function, Quartz clears the current path，所以除非在调用这个方法之前调用`CGContextStrokeRect`，否则不会绘制当前的path了。
+ */
+- (UIImage *)imageTest2WithSize:(CGSize)size {
+    UIGraphicsBeginImageContextWithOptions(size, NO, 0);
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    CGContextSetStrokeColorWithColor(context, [UIColor redColor].CGColor);
+    CGContextMoveToPoint(context, size.width/2, size.height/2);
+    CGContextAddArc(context, size.width/2, size.height/2, floor(size.width/2), 0, M_PI/2, 1);
+    
+    CGRect rect = CGRectMake(0, 0, size.width, size.height);
+    rect = CGRectInset(rect, 10, 10);
+    CGContextStrokeRect(context, rect);
+    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    return image;
+}
+
+// CGContext的绘制都是先依照Quartz的坐标系绘制，然后根据当前context的CTM，将坐标变换成目标值
+
+/**
+ 调用`CGContextFillPath`方法会自动视当前的path为调用CGContextClosePath的结果
+ */
+- (UIImage *)imageTestAutoClosePathWithSize:(CGSize)size {
+    UIGraphicsBeginImageContextWithOptions(size, NO, 0);
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    CGContextSetStrokeColorWithColor(context, [UIColor redColor].CGColor);
+    
+    // 这里如果不调用这个方法，自动close path的时候会视start point为arc的开始点
+    CGContextMoveToPoint(context, size.width/2, size.height/2);
+    
+    CGContextAddArc(context, size.width/2, size.height/2, floor(size.width/2), 0, M_PI/2, 1);
+    CGContextFillPath(context);
+    
+    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    return image;
+}
+
+- (UIImage *)imageTestArcWithSize:(CGSize)size {
+    UIGraphicsBeginImageContextWithOptions(size, NO, 0);
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    CGContextSetStrokeColorWithColor(context, [UIColor redColor].CGColor);
+    
+    CGRect rect = CGRectMake(0, 0, size.width, size.height/2);
+    CGFloat radius = rect.size.height/2;
+    CGContextMoveToPoint(context, radius, 0);
+    CGContextAddArcToPoint(context, 0, 0, 0, radius, radius/2);
+    
+    CGContextMoveToPoint(context, rect.size.width-radius, 0);
+    CGContextAddArcToPoint(context, rect.size.width, 0, rect.size.width, radius, radius);
+    CGContextStrokePath(context);
+    
+    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    return image;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.automaticallyAdjustsScrollViewInsets = NO;
@@ -147,33 +179,37 @@
     [self.view addSubview:_scrollView];
     
     CGFloat y = 0;
+    
+    /// draw a pie style image
     NSArray *percents = @[@10, @20, @30, @40];
     NSArray *colors = @[[UIColor redColor], [UIColor greenColor], [UIColor blueColor], [UIColor yellowColor]];
-//    NSArray *percents = @[@10];
-//    NSArray *colors = @[[UIColor redColor], [UIColor greenColor]];
     UIImage *image = [self imagePieChartWithPercentArray:percents colors:colors imageSize:size];
     UIImageView *imageView = [[UIImageView alloc] initWithImage:image];
     [_scrollView addSubview:imageView];
     imageView.frame = CGRectMake(0, y, imageView.width, imageView.height);
-    imageView.layer.borderWidth = 1;
-    imageView.layer.borderColor = [UIColor redColor].CGColor;
     y = imageView.bottom + 10;
+    
     
     image = [self imageTest1WithSize:size];
     imageView = [[UIImageView alloc] initWithImage:image];
     [_scrollView addSubview:imageView];
     imageView.frame = CGRectMake(0, y, imageView.width, imageView.height);
-    imageView.layer.borderWidth = 1;
-    imageView.layer.borderColor = [UIColor redColor].CGColor;
     y = imageView.bottom + 10;
+    
+    
+    image = [self imageTest1BitmapContextWithSize:size];
+    imageView = [[UIImageView alloc] initWithImage:image];
+    [_scrollView addSubview:imageView];
+    imageView.frame = CGRectMake(0, y, imageView.width, imageView.height);
+    y = imageView.bottom + 10;
+    
     
     image = [self imageTest2WithSize:size];
     imageView = [[UIImageView alloc] initWithImage:image];
     [_scrollView addSubview:imageView];
     imageView.frame = CGRectMake(0, y, imageView.width, imageView.height);
     y = imageView.bottom + 10;
-    imageView.layer.borderWidth = 1;
-    imageView.layer.borderColor = [UIColor redColor].CGColor;
+    
     
     image = [self imageTestAutoClosePathWithSize:size];
     imageView = [[UIImageView alloc] initWithImage:image];
@@ -182,6 +218,7 @@
     imageView.layer.borderWidth = 1;
     imageView.layer.borderColor = [UIColor redColor].CGColor;
     y = imageView.bottom + 10;
+    
     
     image = [self imageTestArcWithSize:size];
     imageView = [[UIImageView alloc] initWithImage:image];
