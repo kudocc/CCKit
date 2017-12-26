0614e5633155e5595969a8b464b22a1fce16b726
kudocc <cangmuma@gmail.com>
2017-12-26 19:20:56 +0800

    update

--- CCKitDemo/textKit/MDLMessageTextStorage.h
+++ CCKitDemo/textKit/MDLMessageTextStorage.h
@@ -6,6 +6,7 @@
 //  Copyright © 2017年 KudoCC. All rights reserved.
 //
 
+#import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 
 extern NSAttributedStringKey MDLMessagetextHighlightedAttributeName;
